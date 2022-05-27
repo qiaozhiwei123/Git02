de187477ca91a2c382cb4125be9bfd7f74b46ef2
qiao <qiao@qq.com>
2022-05-27 22:58:25 +0800

    hot-fix commit

--- src/main/java/com/qiao/git/GitTest.java
+++ src/main/java/com/qiao/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("1111");
         System.out.println("2222");
         System.out.println("3333");
+        System.out.println("hot-fix,44");
     }
 }
