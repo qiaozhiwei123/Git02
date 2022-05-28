81f82165c000f131f4a59ed1a8d78904d050fb36
qiaozhiwei123 <106415026+qiaozhiwei123@users.noreply.github.com>
2022-05-28 09:53:03 +0800

    pull commit

--- src/main/java/com/qiao/git/GitTest.java
+++ src/main/java/com/qiao/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master 555");
         System.out.println("hot-fix,55");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
